8347008a806b6a597149a5d48eac1a12b8a68f63
Michael <michaelalvarado@uagateway.org>
2018-03-11 20:21:28 -0400

    03-10-18

--- signUp.js
+++ signUp.js
@@ -4,10 +4,6 @@ function submitProfile() {
   var firstName = document.querySelector('.user-first-name').value;
   var lastName = document.querySelector('.user-last-name').value;
   var email = document.querySelector('.user-email').value;
-  var photoUrl = document.querySelector('.user-photo').value;
-  var dob = document.querySelector('.user-dob').value;
-  var school = document.querySelector('.user-school').value;
-  var description = document.querySelector('.user-description').value;
 
   if (
     firstName == '' ||
@@ -22,9 +18,6 @@ function submitProfile() {
     localStorage.firstName = firstName;
     localStorage.lastName = lastName;
     localStorage.email = email;
-    localStorage.dob = dob;
-    localStorage.school = school;
-    localStorage.description = description;
 
     location.href = 'home.html';
   }
