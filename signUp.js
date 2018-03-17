4c385c9d1b892c4bf2b786b963b256d675f023c5
Michael <michaelalvarado@uagateway.org>
2018-03-17 12:10:30 -0400

    changed ro index.html

--- signUp.js
+++ signUp.js
@@ -4,6 +4,10 @@ function submitProfile() {
   var firstName = document.querySelector('.user-first-name').value;
   var lastName = document.querySelector('.user-last-name').value;
   var email = document.querySelector('.user-email').value;
+  var photoUrl = document.querySelector('.user-photo').value;
+  var dob = document.querySelector('.user-dob').value;
+  var school = document.querySelector('.user-school').value;
+  var description = document.querySelector('.user-description').value;
 
   if (
     firstName == '' ||
@@ -11,7 +15,8 @@ function submitProfile() {
     email == '' ||
     dob == '' ||
     school == '' ||
-    description == ''
+    description == '' ||
+    photoUrl == ''
   ) {
     document.querySelector('.error').textContent = 'Please fill out all the fields!';
   } else {
@@ -21,7 +26,8 @@ function submitProfile() {
     localStorage.dob = dob;
     localStorage.school = school;
     localStorage.description = description;
+    localStorage.photoUrl = photoUrl;
 
-    location.href = 'home.html';
+    location.href = 'show.html';
   }
 }
