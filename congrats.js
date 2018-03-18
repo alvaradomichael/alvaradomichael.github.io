fd13f3072437b168b75b938dfd58bfa980412f64
Michael <michaelalvarado@uagateway.org>
2018-03-18 17:00:29 -0400

    Super Congrats and Super Shame done

--- congrats.js
+++ congrats.js
@@ -1,1 +1,5 @@
 document.querySelector('.savedBags').textContent = localStorage.numSavedBags;
+
+if (localStorage.numSavedBags == 2){
+  document.querySelector('.supercongrats').style.color = 'black';
+}
--- shame.js
+++ shame.js
@@ -1,1 +1,5 @@
 document.querySelector('.numUsedBags').textContent = localStorage.numUsedBags;
+
+if (localStorage.numUsedBags == 10){
+  document.querySelector('.supershame').style.color = 'black';
+}
